745649985faa4c70199e8e2b2fbdf114a9ca388e
AL-baraa AL-sadeq <89649167+Albaraa-Alsadeq@users.noreply.github.com>
2024-12-18 09:49:52 +0300

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -7,8 +7,6 @@ import cloudinary.api
 
 import os
 
-from pathlib import Path
-
 import django_heroku
 django_heroku.settings(locals())
 
@@ -34,7 +32,8 @@ INSTALLED_APPS = [
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
