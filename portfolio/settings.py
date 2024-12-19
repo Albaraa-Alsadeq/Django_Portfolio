d16f14905fe49bcbb80ca9202cbe4916c59d6e54
AL-baraa AL-sadeq <89649167+Albaraa-Alsadeq@users.noreply.github.com>
2024-12-20 00:53:09 +0300

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -174,7 +174,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Correct root directory for static files
